9d7a4f32c41f2e240e5ba85ce62642462e37a7fe
Tameshwar Nirmalkar <5547424+TameshwarNirmalkar@users.noreply.github.com>
2022-03-22 09:31:59 +0530

    remove warning in console

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
